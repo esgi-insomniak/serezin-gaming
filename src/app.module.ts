f4d45d28513a1faba8ee9e81bca701b722dbd105
Tom Cuillandre <tom.cuillandre@sfr.fr>
2024-11-16 16:11:56 +0100

    feat(postgres): add connection with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { TypeOrmService } from './typeorm.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmService,
+      inject: [TypeOrmService],
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
